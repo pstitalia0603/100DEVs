e4facb195edd3c6a998bca73cc1d87fb4007bd81
pstitalia0603 <paulafrago@gmail.com>
2022-08-16 16:05:35 -0400

    up

--- 031/class31-materials/class31-materials/objects-child-class/js/main.js
+++ 031/class31-materials/class31-materials/objects-child-class/js/main.js
@@ -1,1 +1,57 @@
 //Create an a class and extend it - Can be anything you would like it to be!
+
+class Animal{
+    constructor(name){
+        this._name = name  //private property -- convention that makes it so it's a convention that says do not rewrite -- underscore signifies to not change!
+    }
+    get name(){
+        return this._name
+    }
+    speak(){
+        console.log(`${this._name} makes a sound`)
+    }
+}
+
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this._breed = breed
+    }
+    get breed(){
+        return this._breed
+    }
+    speak(){
+        super.speak()  // from parent
+        console.log(`${this.name} barks`)
+    }
+}
+
+class Cat extends Animal{
+    constructor(name,breed){
+        super(name)
+        this._breed=breed
+    }
+    get breed(){
+        return this._breed
+    }
+    speak(){
+        super.speak()
+        console.log(`${this.name} meows`)
+    }
+}
+
+let simba = new Animal('Simba')
+
+simba.name //"Simba"
+
+simba.name = 'Bob' //nothing happens
+
+let simba2 = new Dog('Simba','Shepard')
+let machi = new Dog('The Machine','Pitbull')
+let salem = new Cat('Salem','American Shorthair')
+
+let farm = [simba2,machi,salem]
+
+for (a of farm){
+    a.speak()
+}
--- 032/class32-materials/objects-practice/js/main.js
+++ 032/class32-materials/objects-practice/js/main.js
@@ -1,6 +1,7 @@
 // Come up with with a parent class
 // Extend that parent class into two children
-// Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
+// Use Encapsulation, Abstraction, Inheritance, and Polymorphism
+
 class Contractor{
     constructor(name,role){
         this._name = name
@@ -13,9 +14,27 @@ class Contractor{
         return this._role
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
+        console.log(`Hello, my name is ${this._name}. I am on the ${this._role} team at #100Devs!`)
     }
 }
+
+// class Contractor{
+//     constructor(name,role){
+//         this._name = name
+//         this._role = role
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     get role(){
+//         return this._role
+//     }
+//     sayHello(){
+//         console.log(`Hello, my name is ${this._name}. I am on the ${this._role} team at #100Devs!`)
+//     }
+// }
+
+
 class Front extends Contractor{
     constructor(name,role,tech){
         super(name,role)
@@ -25,9 +44,25 @@ class Front extends Contractor{
         return this._tech
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+        console.log(`Hello, my name is ${this._name}. I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
     }
 }
+
+
+// class Front extends Contractor{
+//     constructor(name,role,tech){
+//         super(name,role)
+//         this._tech = tech
+//     }
+//     get tech(){
+//         return this._tech
+//     }
+//     sayHello(){
+//         console.log(`Hello, my name is ${this._name}. I am on the ${this._role} team at #100Devs and I use ${this._tech}.`)
+//     }
+// }
+
+
 class Back extends Contractor{
     constructor(name,role,tech){
         super(name,role)
@@ -37,15 +72,40 @@ class Back extends Contractor{
         return this._tech
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+        console.log(`Hello, my name is ${this._name}.I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
     }
 }
-let bob = new Contractor('Bob','Front-end')
-let simba = new Front('Simba','Front-end','React')
-let machi = new Back('The Machine','Back-end','Node')
 
-let agency = [bob,simba,machi]
 
-for(person of agency){
+// class Back extends Contractor{
+//     constructor(name,role,tech){
+//         super(name,role)
+//         this._tech = tech
+//     }
+//     get tech(){
+//         return this._tech
+//     }
+//     sayHello(){
+//         console.log(`Hello, my name is ${this._name}. I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+//     }
+// }
+
+let ella = new Contractor('Ella','front-end')
+let nico = new Front('Nico','frontend','react')
+let gianluca = new Back('Gianluca','fullstack','node')
+
+let agency = [ella,nico,gianluca]
+
+for (person of agency){
     person.sayHello()
 }
+
+// let bob = new Contractor('Bob','Front-end')
+// let simba = new Front('Simba','Front-end','React')
+// let machi = new Back('The Machine','Back-end','Node')
+
+// let agency = [bob,simba,machi]
+
+// for(person of agency){
+//     person.sayHello()
+// }
